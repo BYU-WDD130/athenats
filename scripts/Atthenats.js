057ee3bc9d0784b75c2e10d1c0c17a05585387ef
BYU-WDD130 <henriquezdery@gmail.com>
2026-03-05 20:03:45 -0500

    index

--- scripts/Atthenats.js
+++ scripts/Atthenats.js
@@ -1,33 +1,3 @@
-
-// Open modals
-const modalLinks = document.querySelectorAll('.open-modal');
-const modals = document.querySelectorAll('.modal');
-
-modalLinks.forEach(link => {
-  link.addEventListener('click', function(e) {
-    e.preventDefault();
-    const modalId = this.dataset.modal;
-    document.getElementById(modalId).classList.remove('hidden');
-  });
-});
-
-// Close modals
-modals.forEach(modal => {
-  const closeBtn = modal.querySelector('.close');
-  closeBtn.addEventListener('click', () => {
-    modal.classList.add('hidden');
-  });
-
-  // Close by clicking outside content
-  modal.addEventListener('click', (e) => {
-    if (e.target === modal) {
-      modal.classList.add('hidden');
-    }
-  });
-
-});
-
-
 const navbutton = document.querySelector('#ham-btn');
 const navBar = document.querySelector('#nav-bar');
 
@@ -35,6 +5,3 @@ navbutton.addEventListener('click', () => {
   navbutton.classList.toggle('show');
   navBar.classList.toggle('show');  
 });
-// ---- FOOTER DATES ----
-document.getElementById("currentyear").textContent = new Date().getFullYear();
-document.getElementById("LastModified").textContent = "Last Modified: " + document.lastModified;
